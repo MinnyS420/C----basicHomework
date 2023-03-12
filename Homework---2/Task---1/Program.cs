caac5b3ed64cc616fb8280700d3d9332f36ec2e0
mstamenkovski27@gmail.com <mstamenkovski27@gmail.com>
2023-03-12 21:48:36 +0100

    Homework---2

---
+++ Homework---2/Task---1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Create new console application “SwapNumbers”. Input 2 numbers from the console and then swap the 
+values of the variables so that the first variable has the second value and the second variable the 
+first value. Please find example below:
+
+Test Data:
+Input the First Number: 5
+Input the Second Number: 8
+Expected Output:
+After Swapping:
+First Number: 8
+Second Number: 5
+ */
+
+
+
+Console.Write("Input the First Number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Input the Second Number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Before Swapping:");
+Console.WriteLine("First Number: " + firstNumber);
+Console.WriteLine("Second Number: " + secondNumber);
+
+// Swapping the values of the variables using a temporary variable
+int temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+
+Console.WriteLine("After Swapping:");
+Console.WriteLine("First Number: " + firstNumber);
+Console.WriteLine("Second Number: " + secondNumber);
+
+Console.ReadLine(); // To prevent the console window from closing immediately
---
+++ Homework---2/Task---2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ Make a console application called SumOfEven. Inside it create an array of 6 integers. Get numbers from the input, find and print the sum of the even numbers from the array:
+
+Test Data:
+Enter integer no.1:
+4
+Enter integer no.1:
+3
+Enter integer no.1:
+7
+Enter integer no.1:
+3
+Enter integer no.1:
+2
+Enter integer no.1:
+8
+Expected Output:
+The result is: 14
+ */
+
+
+
+int[] numbers = new int[6];
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("Enter integer no.{0}: ", i + 1);
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sum += number;
+    }
+}
+
+Console.WriteLine("The result is: {0}", sum);
